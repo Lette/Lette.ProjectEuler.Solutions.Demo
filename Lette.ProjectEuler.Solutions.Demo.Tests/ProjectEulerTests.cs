7c91708df7c73d89515546262e3668cf24b4e511
Lette <christoffer.lette@gmail.com>
2014-02-18 01:34:12 +0100

    Removed local EulerDataAttribute, and uses the one in Core.XUnit instead.

--- Lette.ProjectEuler.Solutions.Demo.Tests/ProjectEulerTests.cs
+++ Lette.ProjectEuler.Solutions.Demo.Tests/ProjectEulerTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using Lette.ProjectEuler.Core.XUnit;
+using Xunit;
 using Xunit.Extensions;
 
 namespace Lette.ProjectEuler.Solutions.Demo.Tests
@@ -6,7 +7,7 @@ namespace Lette.ProjectEuler.Solutions.Demo.Tests
     public class ProjectEulerTests
     {
         [Theory]
-        [EulerData]
+        [EulerData("Lette.ProjectEuler.Solutions.Demo.dll")]
         public void RunProblems(string number, string description, long? expected, long actual)
         {
             Assert.True(expected.HasValue, "" + actual + " - Cannot verify - expected value is unknown.");
